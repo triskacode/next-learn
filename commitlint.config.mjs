886d3d307376a7c22d752e47838aaa106d5d0263
Triska Mahfud K <triska.triskacode@gmail.com>
2024-07-21 01:30:39 +0700

    feat: Add linting and code style configuration
    - Install and configure ESLint, Prettier, and Husky
    - Add commitlint for enforcing conventional commit messages
    - Set up lint-staged for pre-commit hooks
    - Create .prettierignore file for excluding specific files/directories
    - Update package.json with new scripts and dev dependencies
    - Add configuration files for ESLint, Prettier, and commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,31 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'release',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+};
+export default config;
